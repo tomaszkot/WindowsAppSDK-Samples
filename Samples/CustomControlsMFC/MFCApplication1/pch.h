fbaf23017f5a09ac220575745b40c193bec99b5f
Koto <tkot@enformatic.com>
2022-10-12 09:06:46 +0200

    trying to compile it

--- Samples/CustomControlsMFC/MFCApplication1/pch.h
+++ Samples/CustomControlsMFC/MFCApplication1/pch.h
@@ -1,17 +1,15 @@
-﻿// pch.h: This is a precompiled header file.
-// Files listed below are compiled only once, improving build performance for future builds.
-// This also affects IntelliSense performance, including code completion and many code browsing features.
-// However, files listed here are ALL re-compiled if any one of them is updated between builds.
-// Do not add files here that you will be updating frequently as this negates the performance advantage.
+﻿#pragma once
 
-#ifndef PCH_H
-#define PCH_H
-
+#include <sdkddkver.h>
 #define _WIN32_LEAN_AND_MEAN
+#define OEMRESOURCE
 // add headers that you want to pre-compile here
 #include "framework.h"
-//#define OEMRESOURCE
 #undef GetCurrentTime
+
+#include <string>
+#include <filesystem>
+#include <memory>
 
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Foundation.Collections.h>
@@ -28,4 +26,3 @@
 #include <wil/result.h>
 
 
-#endif //PCH_H
